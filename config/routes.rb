8be5938f49fa059b24bf6f1ed3ef310041cae911
Jesse vonBergen <jvon1904@gmail.com>
2021-10-04 10:59:28 -0400

    Added blogs link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'greetings/hello'
   resources :blog_posts
+  get 'blogs' => 'blog_posts#index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
