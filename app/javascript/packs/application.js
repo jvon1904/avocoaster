b76dbeef9eae993128f5bb56585de64871d8e657
Jesse vonBergen <jvon1904@gmail.com>
2021-10-05 08:06:56 -0400

    imported bootstrap through the Javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,31 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+import * as bootstrap from "bootstrap";
+import "../stylesheets/application";
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
