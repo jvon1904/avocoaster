40d5d4dc2a72ae395c96ed31a45fc1b321a25810
Jesse vonBergen <jvon1904@gmail.com>
2021-10-12 21:23:21 -0400

    added new Profile scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
+  resources :profiles
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  resources :blog_posts
   get 'profiles/new'
   get 'users/sign_in'
   get 'users/sign_up'
   delete 'users/sign_out'
-  get 'blogs' => 'blog_posts#index'
   get 'about' => 'home#about'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_011224) do
+ActiveRecord::Schema.define(version: 2021_10_13_012137) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_10_13_011224) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
