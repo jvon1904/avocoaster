05b426e310893800caf1cef793731be62e0c2a33
Jesse vonBergen <jvon1904@gmail.com>
2021-10-13 15:15:47 -0400

    modified home page

--- app/views/profiles-old/_profile.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! profile, :id, :first_name, :last_name, :email, :date_of_birth, :created_at, :updated_at
-json.url profile_url(profile, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2021_10_13_012750) do
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_10_13_012750) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
